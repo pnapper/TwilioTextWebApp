b888b9bbcc5a00f2bf8d4520234a052677885415
Daniel Cheng and Patrick Napper <patrick@>
2018-01-22 10:39:39 -0800

    create texting web app using Twilio API.

---
+++ Texter/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Texter.Models;
+
+namespace Texter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetMessages()
+        {
+            var allMessages = Message.GetMessages();
+            return View(allMessages);
+        }
+
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
